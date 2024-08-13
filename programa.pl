6af19bcb4ac0575027722142803a6f5477d9aa46
GonzaloBolana <gbolana@frba.utn.edu.ar>
2024-08-13 13:59:41 -0300

    Primeros Dos puntos

--- programa.pl
+++ programa.pl
@@ -1,1 +1,45 @@
-% Aquí va el código.
+% Cancion, Compositores,  Reproducciones
+cancion(bailanSinCesar, [pabloIlabaca, rodrigoSalinas], 10600177).
+cancion(yoOpino, [alvaroDiaz, carlosEspinoza, rodrigoSalinas], 5209110).
+cancion(equilibrioEspiritual, [danielCastro, alvaroDiaz, pabloIlabaca, pedroPeirano, rodrigoSalinas], 12052254).
+cancion(tangananicaTanganana, [danielCastro, pabloIlabaca, pedroPeirano], 5516191).
+cancion(dienteBlanco, [danielCastro, pabloIlabaca, pedroPeirano], 5872927). 
+cancion(lala, [pabloIlabaca, pedroPeirano], 5100530).
+cancion(meCortaronMalElPelo, [danielCastro, alvaroDiaz, pabloIlabaca, rodrigoSalinas], 3428854).
+
+% Mes, Puesto, Cancion
+rankingTop3(febrero, 1, lala).
+rankingTop3(febrero, 2, tangananicaTanganana).
+rankingTop3(febrero, 3, meCortaronMalElPelo).
+rankingTop3(marzo, 1, meCortaronMalElPelo).
+rankingTop3(marzo, 2, tangananicaTanganana).
+rankingTop3(marzo, 3, lala).
+rankingTop3(abril, 1, tangananicaTanganana).
+rankingTop3(abril, 2, dienteBlanco).
+rankingTop3(abril, 3, equilibrioEspiritual).
+rankingTop3(mayo, 1, meCortaronMalElPelo).
+rankingTop3(mayo, 2, dienteBlanco).
+rankingTop3(mayo, 3, equilibrioEspiritual).
+rankingTop3(junio, 1, dienteBlanco).
+rankingTop3(junio, 2, tangananicaTanganana).
+rankingTop3(junio, 3, lala).
+
+%Punto 1: Saber si una canción es un hit, lo cual ocurre si aparece en el ranking top 3 de todos los meses.
+esUnHit(Cancion):-
+    cancion(Cancion,_,_),
+    forall(rankingTop3(Mes,_,_),rankingTop3(Mes,_,Cancion)).
+    %Para todos los ranking top 3 del mes hay una cancion.
+
+%Punto 2:
+/*
+Saber si una canción no es reconocida por los críticos, 
+lo cual ocurre si tiene muchas reproducciones y nunca estuvo en el ranking. 
+Una canción tiene muchas reproducciones si tiene más de 7000000 reproducciones.
+
+*/
+
+noEsReconocidaPorLosCriticos(Cancion):-
+    cancion(Cancion, _,  Reproducciones),
+    Reproducciones > 7000000.
+    not(rankingTop3(_,_,Cancion)).
+
